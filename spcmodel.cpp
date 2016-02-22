274fc3a3a4457f3f4949ecc0641ad25e0a5edc54
sunpeng <34315439@qq.com>
2016-02-22 22:35:42 +0800

    要提交的变更： 	新文件:       spcmodel.cpp 	新文件:       spcmodel.h  尚未暂存以备提交的变更： 	修改:         main.cpp 	修改:         qttest.pro 	修改:         qttest.pro.user 	删除:         spCdemo 	修改:         spcmodel.h

---
+++ spcmodel.cpp
@@ -0,0 +1,5 @@
+#include "spcmodel.h"
+
+spCmodel::spCmodel()
+{
+}
---
+++ spcmodel.h
@@ -0,0 +1,10 @@
+#ifndef SPCMODEL_H
+#define SPCMODEL_H
+
+class spCmodel
+{
+public:
+    spCmodel();
+};
+
+#endif // SPCMODEL_H
